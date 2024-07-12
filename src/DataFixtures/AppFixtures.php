3c412d2c09406fb38d7c6379b4f421816d9aed96
binyoma <innocentbinyoma@pm.me>
2024-07-12 12:21:38 +0200

    relations in foundry

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Answer;
 use App\Entity\Question;
+use App\Factory\AnswerFactory;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -12,24 +13,18 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        QuestionFactory::createMany(20);
+        $questions = QuestionFactory::createMany(20);
 
         QuestionFactory::new()
             ->unpublished()
             ->many(5)
             ->create()
         ;
-
-        $answer = new Answer();
-        $answer->setContent('This question is the best! I wish I knew the answer! ');
-        $answer->setUsername('Innocent');
-
-        $question = new Question();
-        $question->setName('How to un-disappear your wallet.');
-        $question->setQuestion('...I should not have done this..');
-        $answer->setQuestion($question);
-        $manager->persist($answer);
-        $manager->persist($question);
+        AnswerFactory::createMany(100, function() use ($questions){
+            return [
+                'question'=>$questions[array_rand($questions)],
+            ];
+        }); 
 
         $manager->flush();
     }
